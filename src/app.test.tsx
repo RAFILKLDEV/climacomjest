4422740698b13a734766566c1be624e06dbe9bdb
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2023-03-15 16:25:25 -0300

    aprendendo Jest

---
+++ src/app.test.tsx
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react";
+import { Climate } from "./components/Climate";
+
+describe("Climate Component", () => {
+  test("Deve conter o name Tókyo", () => {});
+  render()
+});
