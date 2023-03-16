354b8d872b9ba96a13acdcc39a0fc9458c1acb92
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2023-03-16 18:56:02 -0300

    Aprendendo um pouco mais sobre tests

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,7 +1,13 @@
-import { render } from "@testing-library/react";
-import { Climate } from "./components/Climate";
+import { render, screen } from "@testing-library/react";
+import { Climate } from "@/components/Climate";
+
+const mock =
+  "https://api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&current_weather=true&timezone=America%2FSao_Paulo";
 
 describe("Climate Component", () => {
-  test("Deve conter o name Tókyo", () => {});
-  render()
+  test("Deve conter o name Tókyo", async () => {
+    render(<Climate name={"Tókyo"} />);
+    const label = screen.getByText("Tókyo");
+    expect(label).toBeInTheDocument();
+  });
 });
